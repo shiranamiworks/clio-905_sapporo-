196b77fddc38abe0ae47aec596512b48b0bce5a9
yoshino umemi <umemi.0401@gmail.com>
2025-03-04 13:46:01 +0900

    no message

--- index.php
+++ index.php
@@ -150,6 +150,11 @@ require "../cookie.php";
           <div class="fade-in fade-in-up">
             <h2 class="ttl CormorantGaramond">INFORMATION</h2>
             <!-- <p class="link"><a href="https://www.youtube.com/watch?v=0m9-vNEI0fs" target="_blank" class="gtm_click_trg" id="view_movie_kengaku"><span>[安心見学動画公開]</span><br><span>ご案内時の感染予防対策について</span></a></p> -->
+            <div class="topic">
+              <p class="txt-open">3/3(月)～3/8(土)限定</p>
+              <p class="txt-open02">建物内覧会開催<br>～実際のお部屋を<br class="sp">ご覧いただけます～<br><span class="fs90">＜完全予約制＞</span></p>
+              <p class="mini">※詳しくは係員までお問い合わせください。</p>
+            </div>
             <p class="price"><span class="num">1</span>LDK / <span class="num">36</span>㎡超<br><span class="red">2,900</span>万円台〜 <br class="sp"><span class="mini">※100万円単位</span></p>
             <div class="open">
               <p class="open-time">第1期1次先着順申込受付中</p>
